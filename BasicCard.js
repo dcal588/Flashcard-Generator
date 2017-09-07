e02004797ec2caa7c6e1a9da695c286e35f00115
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-09-07 01:36:08 -0700

    Function Fix

---
+++ BasicCard.js
@@ -0,0 +1,27 @@
+var inquirer = require('inquirer');
+
+function BasicCard (front, back) {
+  this.front = front;
+  this.back = back;
+}
+BasicCard.prototype.printInfo = function(front, back) {
+  console.log("Front of Card: " + this.front)
+  console.log("Back of Card: " + this.back)
+};
+inquirer.prompt([
+  {
+   	type: "input",
+   	message: "Provide text for front of card: ",
+   	name: "front"
+  },
+  {
+   	type: "input",
+   	message: "Provide text for back of card: ",
+   	name: "back",
+  }
+])
+.then(function(inquirerResponse) {
+  var firstPresident = new BasicCard(inquirerResponse.front, inquirerResponse.back)
+  firstPresident.printInfo();
+});
+module.exports = BasicCard;
