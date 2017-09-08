4a477d6efcbe843bd7b9125ca493b95a09602c52
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-09-07 19:18:18 -0700

    Final Changes

--- ClozeCard.js
+++ ClozeCard.js
@@ -4,27 +4,26 @@ function ClozeCard (cloze, text) {
   this.cloze = cloze;
   this.text = text;
   this.fullText = cloze + " " + text;
-  inquirer.prompt([
+}
+ClozeCard.prototype.printInfo = function(cloze, text) {
+  console.log("Cloze Text: " + this.cloze);
+  console.log("Partial Text : " + this.text);
+  console.log("Full Text : " + this.fullText);
+};
+inquirer.prompt([
   {
-    type: "input",
-    message: "Provide Cloze Text: ",
-    name: "cloze"
+   	type: "input",
+   	message: "Provide Cloze Text: ",
+   	name: "cloze"
   },
   {
-    type: "input",
-    message: "Provide Partial Text: ",
-    name: "text",
+   	type: "input",
+   	message: "Provide Partial Text: ",
+   	name: "text",
   }
 ])
 .then(function(inquirerResponse) {
   var firstPresident = new ClozeCard(inquirerResponse.cloze, inquirerResponse.text)
   firstPresident.printInfo();
 });
-}
-ClozeCard.prototype.printInfo = function(front, back) {
-  console.log("Cloze Text: " + this.cloze);
-  console.log("Partial Text : " + this.text);
-  console.log("Full Text : " + this.fullText);
-};
-
 module.exports = ClozeCard;
--- cli.js
+++
@@ -1,9 +0,0 @@
-var basic = require("./BasicCard.js");
-var cloze = require("./ClozeCard.js");
-
-if (process.argv[2]==="basic") {
-	basicBasicCard();
-}
-if (process.argv[2]==="cloze") {
-	ClozeCard();
-}
